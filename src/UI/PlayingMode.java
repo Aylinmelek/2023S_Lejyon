edee105595c7e8926c3068685406d10353213031
bkulek20 <72880656+bkulek20@users.noreply.github.com>
2023-05-11 02:56:45 +0300

    grid and army cards changes

--- src/UI/PlayingMode.java
+++ src/UI/PlayingMode.java
@@ -8,53 +8,54 @@ import javax.swing.JButton;
 import javax.swing.JLayeredPane;
 
 public class PlayingMode extends JLayeredPane {
-	Grid map = new Grid();
+	
 	JButton btnTer = new JButton("Territory Cards");
 	JButton btnArmy = new JButton("Army Cards");
 	JButton btnChance = new JButton("Chance Cards");
 	
 	public void initialize() {
 		setBackground(Color.DARK_GRAY);
+		
+		
+		
 		setBounds(0, 54, 873, 451);
 		
 		setLayout(null);
 	}
 	
 	public void addElements() {
-		map.addGrid();
-		add(map);
-		
-	     btnTer.addActionListener(new ActionListener() {
-	     	public void actionPerformed(ActionEvent e) {
-	     		territoryCardFrame territoryCard = new territoryCardFrame();
-	     		territoryCard.territoryCardF.setVisible(true);
-	
-	     	}
-	     });
-	     btnTer.setBounds(27, 337, 118, 29);
-	     add(btnTer);
-	     
-	     
-	     btnArmy.addActionListener(new ActionListener() {
-	     	public void actionPerformed(ActionEvent e) {
-	     		ArmyCardFrame armyCard = new ArmyCardFrame();
-	     		armyCard.ArmyCardF.setVisible(true);
-	
-	     	}
-	     });
-	     btnArmy.setBounds(27, 367, 118, 29);
-	     add(btnArmy);
-	     
-	     
-	     btnChance.addActionListener(new ActionListener() {
-	     	public void actionPerformed(ActionEvent e) {
-	     		ChanceCardFrame armyCard = new ChanceCardFrame();
-	     		armyCard.chanceCardF.setVisible(true);
-	     	}
-	     });
-	     btnChance.setBounds(27, 397, 118, 29);
-	     add(btnChance);
-	     
+     btnTer.addActionListener(new ActionListener() {
+     	public void actionPerformed(ActionEvent e) {
+     		territoryCardFrame territoryCard = new territoryCardFrame();
+     		territoryCard.territoryCardF.setVisible(true);
+
+     	}
+     });
+     btnTer.setBounds(27, 437, 118, 30);
+     add(btnTer);
+     
+     
+     btnArmy.addActionListener(new ActionListener() {
+     	public void actionPerformed(ActionEvent e) {
+     		ArmyCardFrame armyCard = new ArmyCardFrame();
+     		armyCard.ArmyCardF.setVisible(true);
+
+     	}
+     });
+     
+     btnArmy.setBounds(27, 467, 118, 29);
+     add(btnArmy);
+     
+     
+     btnChance.addActionListener(new ActionListener() {
+     	public void actionPerformed(ActionEvent e) {
+     		ChanceCardFrame armyCard = new ChanceCardFrame();
+     		armyCard.chanceCardF.setVisible(true);
+     	}
+     });
+     btnChance.setBounds(27, 497, 118, 29);
+     add(btnChance);
+     
 	}
 
 }
