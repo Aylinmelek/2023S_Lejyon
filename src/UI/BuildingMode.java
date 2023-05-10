e9a69a7424aaf674bceccac3db9f322fb571fd03
bkulek20 <72880656+bkulek20@users.noreply.github.com>
2023-05-11 02:54:33 +0300

    grid and army cards updates

--- src/UI/BuildingMode.java
+++ src/UI/BuildingMode.java
@@ -12,16 +12,17 @@ import javax.swing.JLayeredPane;
 import javax.swing.JTextArea;
 
 public class BuildingMode extends JLayeredPane {
-	Grid map = new Grid();
 	JTextArea txtrGameOptions = new JTextArea();
-    JComboBox<Integer> numPlayers = new JComboBox<Integer> ();
-    JComboBox<Integer> numComp = new JComboBox<Integer>();
+    static JComboBox<Integer> numPlayers = new JComboBox<Integer> ();
+    static JComboBox<Integer> numComp = new JComboBox<Integer>();
     JTextArea txtnumPlayers = new JTextArea();
     JTextArea txtnumComp = new JTextArea();
     JButton btnNext = new JButton("Next");
     PlayingMode playingMode = new PlayingMode();
     LoginScreen loginScreen = new LoginScreen();
     InitSharing initSharing = new InitSharing();
+    static String players,comp;
+    static int numOfInfantry, totalPlayers=0;
 
 	public void initialize()  {
 		setBounds(0, 54, 873, 451);
@@ -30,10 +31,8 @@ public class BuildingMode extends JLayeredPane {
 	    setLayout(null);
 	}
 	
+	
 	public void addElements() {
-		map.addGrid();
-		add(map);
-		//map.setVisible(true);
 		
         txtrGameOptions.setBounds(212, 6, 453, 43);
         txtrGameOptions.setText("Game Options & Map Settings");
@@ -60,23 +59,56 @@ public class BuildingMode extends JLayeredPane {
         add(txtnumComp);
         
         numPlayers.setBounds(432, 361, 64, 27);
-        numPlayers.setModel(new DefaultComboBoxModel<Integer>(new Integer[] {2, 3, 4, 5, 6}));
-        //numPlayers.setToolTipText("");
+        numPlayers.setModel(new DefaultComboBoxModel<Integer>(new Integer[] {0, 2, 3, 4, 5, 6}));
+    
         numPlayers.setMaximumRowCount(6);
+   
         numPlayers.setBackground(Color.LIGHT_GRAY);
         add(numPlayers);
-        
+       
+
         numComp.setBounds(432, 391, 64, 27);
-        numComp.setModel(new DefaultComboBoxModel<Integer>(new Integer[] {1, 2, 3, 4, 5, 6}));
+        numComp.setModel(new DefaultComboBoxModel<Integer>(new Integer[] {0, 1, 2, 3, 4, 5, 6}));
         numComp.setMaximumRowCount(6);
         numComp.setBackground(Color.LIGHT_GRAY);
         add(numComp);
-        
-        
 
         btnNext.setBounds(724, 404, 117, 29);
         add(btnNext);
         
+	}
+	
+	public static int armyNum() {  
+		
+	
+	    players = String.valueOf(numPlayers.getSelectedItem().toString());
+		System.out.println(players); 
+		
+		comp = String.valueOf(numComp.getSelectedItem().toString());
+		System.out.println(comp); 
+		
+		totalPlayers = Integer.parseInt(players) + Integer.parseInt(comp);
+	
+		System.out.println(totalPlayers);
+		if (totalPlayers == 2) {
+			numOfInfantry = 40;
+		}
+	    if (totalPlayers == 3) {
+			numOfInfantry = 35;
+		}
+		if (totalPlayers == 4) {
+			numOfInfantry = 30;
+		}
+		if (totalPlayers == 5) {
+			numOfInfantry = 25;
+		}
+		if (totalPlayers == 6) {{
+			numOfInfantry = 20;
+		}
+	
+	}
+		return numOfInfantry;
+
 	}
 
 }
