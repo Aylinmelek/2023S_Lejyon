f16695c5426bfd103a3099e3a6556c6e577379f4
KesenBaris <kkesen20@ku.edu.tr>
2023-05-25 16:22:54 +0300

    Fortifying Method bug fixes
    Kemal Barış Kesen
    Betül Korkmaz

--- src/domain/ConKUeror.java
+++ src/domain/ConKUeror.java
@@ -5,66 +5,69 @@ import java.util.ArrayList;
 import UI.Grid;
 
 public class ConKUeror {
-	ArrayList <Player> playerList = new ArrayList<Player>();
+	ArrayList<Player> playerList = new ArrayList<Player>();
 	Grid grid = new Grid();
 	Die die = new Die();
+
 	public void attack(Player player, Territory territoryFrom, Territory territoryTo, Die die) {
-		if (player.canAttackTerritory(territoryFrom, territoryTo) && territoryTo.adjacentTerritories.contains(territoryFrom)) {
+		if (player.canAttackTerritory(territoryFrom, territoryTo)
+				&& territoryTo.adjacentTerritories.contains(territoryFrom)) {
 			System.out.println(player + " is attacking now.");
-			if(die.generateNum() > die.generateNum()) {
+			if (die.generateNum() > die.generateNum()) {
 				territoryTo.owner.loseTheDefend(territoryTo);
 				System.out.println(player + " lose the defend.");
-			}
-			else {
+			} else {
 				territoryFrom.owner.loseTheAttack(territoryFrom);
 				System.out.println(player + " lose the attack.");
 			}
-			if(territoryTo.armyList.size() <= 0) {
+			if (territoryTo.armyList.size() <= 0) {
 				player.conquerTerritory(territoryTo);
 				System.out.println(player + " conquer the territory.");
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	public void fortify(Player player, Territory territoryFrom, Territory territoryTo, Integer count) {
-		
-			if(territoryFrom.getArmyList().size() >= count) {
-				for(int i = 0; i < count; i++) {
+
+		if (player.getTerritoryList().contains(territoryTo) && player.getTerritoryList().contains(territoryFrom)
+				&& territoryFrom.getAdjacentTerritories(territoryTo))
+			if (territoryFrom.getArmyList().size() >= count) {
+				for (int i = 0; i < count; i++) {
 					int index = territoryFrom.getArmyList().size() - 1;
-					System.out.println(territoryFrom.getArmyList().get(index) + "is fortified from" + territoryFrom + "to" + territoryTo);
+					System.out.println(territoryFrom.getArmyList().get(index) + "is fortified from" + territoryFrom
+							+ "to" + territoryTo);
 					player.fortifying(territoryFrom, territoryTo, territoryFrom.getArmyList().get(index));
 				}
 			}
-			
-		}
-	
+
+	}
+
 	public void initialSharingOfTerritory(ArrayList<Player> playerList, Map map) {
 		boolean workUntil = true;
 		boolean allTrue = false;
-		while(workUntil) {
-			if(!allTrue) {
-				//System.out.println(playerList.size());
-				for(int i = 0; i < playerList.size(); i++) {
-				//	System.out.println(grid.getRow());
-					//playerList.get(i).chooseATerritory();
-							/*UI da seçtiği territoryi buraya parametre olarak yolluyoruz*/ 
+		while (workUntil) {
+			if (!allTrue) {
+				// System.out.println(playerList.size());
+				for (int i = 0; i < playerList.size(); i++) {
+					// System.out.println(grid.getRow());
+					// playerList.get(i).chooseATerritory();
+					/* UI da seçtiği territoryi buraya parametre olarak yolluyoruz */
 					System.out.println(playerList.get(i) + " will choose a territory.");
 					map.getTerritories().get(i).isTaken = true;
 					allTrue = true;
-					for (Territory territory: map.territories) {
-					    if (!territory.isTaken) {
-					        allTrue = false;
-					        break;
-					    }
+					for (Territory territory : map.territories) {
+						if (!territory.isTaken) {
+							allTrue = false;
+							break;
+						}
 					}
 				}
-			}
-			else {
+			} else {
 				workUntil = false;
 			}
 		}
-		
+
 	}
 }
