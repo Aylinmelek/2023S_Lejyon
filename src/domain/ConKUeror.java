a1e452ff32f7e9eca3e6c22a15236ebb118514cd
KesenBaris <kkesen20@ku.edu.tr>
2023-05-25 17:17:58 +0300

    Fortifying Tests
    Kemal Barış Kesen
    Betül Korkmaz

--- src/domain/ConKUeror.java
+++ src/domain/ConKUeror.java
@@ -9,6 +9,7 @@ public class ConKUeror {
 	Grid grid = new Grid();
 	Die die = new Die();
 	public void attack(Player player, Territory territoryFrom, Territory territoryTo, Die die) {
+<<<<<<< Updated upstream
 		if (player.canAttackTerritory(territoryFrom, territoryTo) && territoryTo.adjacentTerritories.contains(territoryFrom)) {
 			System.out.println(player + " is attacking now.");
 			if(die.generateNum() > die.generateNum()) {
@@ -24,22 +25,70 @@ public class ConKUeror {
 				System.out.println(player + " conquer the territory.");
 			}
 			
+=======
+
+		if (player.getTerritoryList().contains(territoryFrom) && !player.getTerritoryList().contains(territoryTo)) {
+			if (player.canAttackTerritory(territoryFrom, territoryTo)
+					&& territoryTo.adjacentTerritories.contains(territoryFrom)) {
+				System.out.println(player + " is attacking now.");
+				if (die.generateNum() > die.generateNum()) {
+					territoryTo.owner.loseTheDefend(territoryTo);
+					System.out.println(player + " lose the defend.");
+				} else {
+					territoryFrom.owner.loseTheAttack(territoryFrom);
+					System.out.println(player + " lose the attack.");
+				}
+				if (territoryTo.armyList.size() <= 0) {
+					player.conquerTerritory(territoryTo);
+					System.out.println(player + " conquer the territory.");
+				}
+
+			} else {
+				System.out.println("Can not attack");
+			}
+
+		} else {
+			System.out.println("Can not attack");
+>>>>>>> Stashed changes
 		}
 		
 	}
 	
 	public void fortify(Player player, Territory territoryFrom, Territory territoryTo, Integer count) {
+<<<<<<< Updated upstream
 		
 			if(territoryFrom.getArmyList().size() >= count) {
 				for(int i = 0; i < count; i++) {
 					int index = territoryFrom.getArmyList().size() - 1;
 					System.out.println(territoryFrom.getArmyList().get(index) + "is fortified from" + territoryFrom + "to" + territoryTo);
+=======
+
+		if (player.getTerritoryList().contains(territoryTo) && player.getTerritoryList().contains(territoryFrom)
+				&& territoryFrom.getAdjacentTerritories().contains(territoryTo)) {
+			if (territoryFrom.getArmyList().size() >= count) {
+				for (int i = 0; i < count; i++) {
+					int index = territoryFrom.getArmyList().size() - 1;
+					System.out.println(territoryFrom.getArmyList().get(index) + "is fortified from " + territoryFrom
+							+ "to" + territoryTo);
+>>>>>>> Stashed changes
 					player.fortifying(territoryFrom, territoryTo, territoryFrom.getArmyList().get(index));
 				}
+			} else {
+				System.out.println("Can not fortify");
 			}
+<<<<<<< Updated upstream
 			
 		}
 	
+=======
+
+		} else {
+			System.out.println("Can not fortify");
+		}
+
+	}
+
+>>>>>>> Stashed changes
 	public void initialSharingOfTerritory(ArrayList<Player> playerList, Map map) {
 		boolean workUntil = true;
 		boolean allTrue = false;
@@ -49,7 +98,11 @@ public class ConKUeror {
 				for(int i = 0; i < playerList.size(); i++) {
 				//	System.out.println(grid.getRow());
 					//playerList.get(i).chooseATerritory();
+<<<<<<< Updated upstream
 							/*UI da seçtiği territoryi buraya parametre olarak yolluyoruz*/ 
+=======
+							/UI da seçtiği territoryi buraya parametre olarak yolluyoruz/ 
+>>>>>>> Stashed changes
 					System.out.println(playerList.get(i) + " will choose a territory.");
 					map.getTerritories().get(i).isTaken = true;
 					allTrue = true;
---
+++ src/domain/FortifyTest.java
@@ -0,0 +1,47 @@
+package domain;
+
+public class FortifyTest {
+    public static void main(String[] args) {
+        Player player1 = new Player();
+        Player player2 = new Player();
+
+        Territory territory1 = new Territory();
+        Territory territory2 = new Territory();
+        Territory territory3 = new Territory();
+        Territory territory4 = new Territory();
+        territory1.getAdjacentTerritories().add(territory2);
+        territory2.getAdjacentTerritories().add(territory1);
+        territory1.getAdjacentTerritories().add(territory4);
+        territory3.getAdjacentTerritories().add(territory4);
+        territory4.getAdjacentTerritories().add(territory3);
+
+        Infantry infantry1 = new Infantry();
+        Infantry infantry2 = new Infantry();
+
+        territory1.setOwner(player1);
+        territory2.setOwner(player1);
+        territory3.setOwner(player1);
+        territory4.setOwner(player2);
+
+        player1.getTerritoryList().add(territory1);
+        player1.getTerritoryList().add(territory2);
+        player1.getTerritoryList().add(territory3);
+        player2.getTerritoryList().add(territory4);
+        territory1.getArmyList().add(infantry1);
+        territory1.getArmyList().add(infantry2);
+
+        ConKUeror conkueror = new ConKUeror();
+
+        System.out.println("1:");
+        conkueror.fortify(player1, territory2, territory1, 2); // If army count of a territory is = 0
+        System.out.println("2:");
+        conkueror.fortify(player1, territory1, territory3, 2); // If territoryTo is not adjacent of TerritoryFrom
+        System.out.println("3:");
+        conkueror.fortify(player1, territory1, territory4, 2); // If Player doesn't own territoryTo
+        System.out.println("4:");
+        conkueror.fortify(player1, territory1, territory4, 2); // If Player doesn't own territoryFrom
+        System.out.println("5:");
+        conkueror.fortify(player1, territory1, territory2, 2); // Working Test Case
+
+    }
+}
