d16abaaad784a73d93fd15a61c5ed12739f18772
KesenBaris <kkesen20@ku.edu.tr>
2023-06-01 17:18:17 +0300

    Bug Fixes
    Kemal Barış Kesen
    Betül Korkmaz

--- src/domain/ConKUeror.java
+++ src/domain/ConKUeror.java
@@ -9,12 +9,8 @@ import UI.LoginScreen;
 
 public class ConKUeror {
 
-	public ArrayList<Player> playerList = new ArrayList<Player>();
-	public ArrayList<Territory> terList = new ArrayList<Territory>();
-	public InfantryCreator infCreator = new InfantryCreator();
-	public PlayerCreator playerCreator = new PlayerCreator();
-	public MainDeckCreator mainDeckCreator = new MainDeckCreator();
-	public GameActions gameActions = new GameActions();
+	
+	
 	
 	Grid grid = new Grid();
 	public Die die = new Die();
@@ -22,46 +18,7 @@ public class ConKUeror {
 	public Hashtable<Player, Boolean> player_turn = new Hashtable<Player, Boolean>();
 
 	
-	public void createInfantry(int number, Player player) {
-		infCreator.createInfantry(number,Player);
-	}
 	
-	public void createPlayer(int playerCount, int AICount) {
-		playerCreator.createPlayer(playerCount,AICount);
-	}
-	
-	public void createMainDeck(int count, int armyCard, Deck deck) {
-		mainDeckCreator.createMainDeck(count, armyCard, deck);
-	}
-
-	/*public void setAdjacent(Map map) {
-		for (int i = 0; i < map.getTerritories().size(); i++) {
-			for (int j = 0; j < map.getTerritories().size(); j++) {
-				map.getTerritories().get(i).getAdjacentTerritories().add(map.getTerritories().get(j));
-				
-			}
-		}
-	}*/
-	
-	public void turnPass(int index) {
-		gameActions.turnPass(index);
-	}
-
-	public void createTerritory(int terCount) {
-		gameActions.createTerritory(terCount);
-	}
-
-	public void attack(Player player, Territory territoryFrom, Territory territoryTo, Die die) {
-
-		gameActions.attack(player, territoryFrom, territoryTo, die);
-
-	}
-
-	public void fortify(Player player, Territory territoryFrom, Territory territoryTo, Integer count) {
-
-		gameActions.fortify(player, territoryFrom, territoryTo, count);
-
-	}
 
 	public void initialSharingOfTerritory(ArrayList<Player> playerList, Map map) {
 		boolean workUntil = true;
--- src/domain/controller/ConKUerorHandler.java
+++ src/domain/controller/ConKUerorHandler.java
@@ -7,6 +7,8 @@ import domain.Board;
 import domain.Die;
 
 public class ConKUerorHandler {
+
+	
 	
 private Board board;
 	
@@ -32,6 +34,53 @@ private Board board;
 	
 	public int getDieValue() {
 		return board.getDiceValue();
+	}
+
+	//New Part
+	public InfantryCreator infCreator = new InfantryCreator();
+	public PlayerCreator playerCreator = new PlayerCreator();
+	public MainDeckCreator mainDeckCreator = new MainDeckCreator();
+	public GameActions gameActions = new GameActions();
+	
+	public void createInfantry(int number, Player player) {
+		infCreator.createInfantry(number,Player);
+	}
+	
+	public void createPlayer(int playerCount, int AICount) {
+		playerCreator.createPlayer(playerCount,AICount);
+	}
+	
+	public void createMainDeck(int count, int armyCard, Deck deck) {
+		mainDeckCreator.createMainDeck(count, armyCard, deck);
+	}
+
+	/*public void setAdjacent(Map map) {
+		for (int i = 0; i < map.getTerritories().size(); i++) {
+			for (int j = 0; j < map.getTerritories().size(); j++) {
+				map.getTerritories().get(i).getAdjacentTerritories().add(map.getTerritories().get(j));
+				
+			}
+		}
+	}*/
+	
+	public void turnPass(int index) {
+		gameActions.turnPass(index);
+	}
+
+	public void createTerritory(int terCount) {
+		gameActions.createTerritory(terCount);
+	}
+
+	public void attack(Player player, Territory territoryFrom, Territory territoryTo, Die die) {
+
+		gameActions.attack(player, territoryFrom, territoryTo, die);
+
+	}
+
+	public void fortify(Player player, Territory territoryFrom, Territory territoryTo, Integer count) {
+
+		gameActions.fortify(player, territoryFrom, territoryTo, count);
+
 	}
 	
 
