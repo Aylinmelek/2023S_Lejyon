365cebfe4aaeccb0e715587e35c2c81b52e7c0ff
aylinmelek <aylinmelek@172.20.10.3>
2023-06-04 18:41:18 +0300

    specification of the tests are added

--- src/test/PlaceArmyTesting.java
+++ src/test/PlaceArmyTesting.java
@@ -10,6 +10,12 @@ import domain.Player;
 import domain.Territory;
 
 public class PlaceArmyTesting {
+	// Specification:
+	// @Requires: Player should be in the initial sharing and army placement part of the game.
+	// @Modifies: territoryList, InfantryList, ArmyList
+	// @Effects: If a player has a territory the player can add infantries to that territory 
+	//in the initial sharing and army placing part of the game
+	
 
 	Player player = new Player();
 	Player player2 = new Player();
@@ -23,18 +29,27 @@ public class PlaceArmyTesting {
 	}
 
 	@Test
+	public void armyNotPlaced() {
+		player.getTerritoryList().add(territory); // territory added to player's territory list
+		player.placeArmy(territory, "Infantry"); // player placed army to that territory
+		assertTrue(territory.getArmyList().isEmpty()); // Check if the territory in the army list is empty
+	}
+
+	@Test
+	public void territoryNotInTerList() {
+		// territory is not in the players territory list
+		player.placeArmy(territory, "Infantry"); // player placed infantry to that territory
+
+		// Check if the territory is empty
+		assertTrue(territory.getArmyList().isEmpty()); // if the territory is empty, it returns true
+	}
+
+	@Test
 	public void invalidArmyType() {
 		player.getTerritoryList().add(territory); // territory added to players territorylist
 		player.placeArmy(territory, "Cavalry"); // player places army in that territory
 
 		assertTrue(territory.getArmyList().isEmpty()); // returns true if the territory's army list is empty
-	}
-
-	@Test
-	public void armyNotPlaced() {
-		player.getTerritoryList().add(territory); // territory added to player's territory list
-		player.placeArmy(territory, "Infantry"); // player placed army to that territory
-		assertTrue(territory.getArmyList().isEmpty()); // Check if the territory in the army list is empty
 	}
 
 	@Test
@@ -49,15 +64,6 @@ public class PlaceArmyTesting {
 		player.placeArmy(territory, "Infantry"); // player placed infantry to that territory
 
 		assertEquals(2, player.getInfantryList().size()); // returns true if the players army list is equal to 2
-	}
-
-	@Test
-	public void territoryNotInTerList() {
-		// territory is not in the players territory list
-		player.placeArmy(territory, "Infantry"); // player placed infantry to that territory
-
-		// Check if the territory is empty
-		assertTrue(territory.getArmyList().isEmpty()); // if the territory is empty, it returns true
 	}
 
 }
